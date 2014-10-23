db320c062126b59dc4c4985b132a94d6cbc4e163
iice <sascha.siemer@gmx.de>
2014-10-23 17:09:32 +0200

    initial commit

--- admin/server/php/databaseAccess/live_options.php
+++ admin/server/php/databaseAccess/live_options.php
@@ -3,7 +3,7 @@
 $options = array(
 	'delete_type' => 'POST',
 	'db_host' => 'localhost',
-	'db_user' => 'root',
+	'db_user' => 'rootttt',
 	'db_pass' => '',
 	'db_name' => 'photoblog',
 	'db_table' => 'images'
