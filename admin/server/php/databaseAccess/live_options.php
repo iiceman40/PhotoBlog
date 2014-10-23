4ce3ae7ee34b8cf4c99af99fb822ad08fc5b299c
iice <sascha.siemer@gmx.de>
2014-10-23 17:15:16 +0200

    initial commit

--- admin/server/php/databaseAccess/live_options.php
+++ admin/server/php/databaseAccess/live_options.php
@@ -3,7 +3,7 @@
 $options = array(
 	'delete_type' => 'POST',
 	'db_host' => 'localhost',
-	'db_user' => 'rootttt',
+	'db_user' => 'root',
 	'db_pass' => '',
 	'db_name' => 'photoblog',
 	'db_table' => 'images'
