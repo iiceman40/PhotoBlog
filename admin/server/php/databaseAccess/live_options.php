b1a13cd7835bff65a5f4b339a200f178582c01ce
iice <sascha.siemer@gmx.de>
2014-10-23 17:07:34 +0200

    initial commit

---
+++ admin/server/php/databaseAccess/live_options.php
@@ -0,0 +1,11 @@
+<?php
+// LIVE DB CONNECTION
+$options = array(
+	'delete_type' => 'POST',
+	'db_host' => 'localhost',
+	'db_user' => 'root',
+	'db_pass' => '',
+	'db_name' => 'photoblog',
+	'db_table' => 'images'
+);
+?>
